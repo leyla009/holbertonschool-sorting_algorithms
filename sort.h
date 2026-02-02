d2b59e636b895630f1b5b849345250b608f25b9a
Leyla <Leyla.khaspolad@gmail.com>
2026-02-02 12:22:50 +0000

    Fix: Update the sort.h file for the Betty style compliance

--- sort.h
+++ sort.h
@@ -13,16 +13,16 @@
  */
 typedef struct listint_s
 {
-	const int n;         
-	struct listint_s *prev; 
-	struct listint_s *next; 
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
-/* Helper function */
+/* Helper functions provided */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
-/* Mandatory Sorting Algorithms */
+/* Algorithms */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
